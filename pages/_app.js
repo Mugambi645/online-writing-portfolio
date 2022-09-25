a872ee6cdf898fe1f7ee61b5f3063ed5b7f68017
patrick mugambi <patrick@kali.kali>
2022-09-26 00:35:51 +0300

    create and register component hero

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import Hero from '../components/home/Hero'
 function MyApp({ Component, pageProps }) {
   return (
     <>
     <Layout>
     <Component {...pageProps}/>
+    <Hero/>
     </Layout>
     </>
      )
