88eae4fa36c2b3d31a34e077976d147db055d1a1
patrick mugambi <patrick@kali.kali>
2022-09-26 01:23:56 +0300

    correct wrong component import

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-import Hero from '../components/home/Hero'
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
     <Layout>
     <Component {...pageProps}/>
-    <Hero/>
+
     </Layout>
     </>
      )
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Hero from '../components/home/Hero'
 export default function Home() {
   return (
   <div>
-
+<Hero/>
     </div>
   )
 }
