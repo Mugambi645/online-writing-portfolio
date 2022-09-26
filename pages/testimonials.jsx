e123fb810f99322363770f2acdcd74fbc019ddf6
patrick mugambi <patrick@kali.kali>
2022-09-26 09:38:19 +0300

    add testimonial layout

--- pages/testimonials.jsx
+++ pages/testimonials.jsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import MoreInfo from '../components/home/MoreInfo'
 import Hero from '../components/testimonials/Hero'
+import TestimonialComponent from '../components/testimonials/TestimonialComponent'
 export default function Testimonials() {
   return (
   <div>
 <Hero/>
+<TestimonialComponent/>
 <MoreInfo/>
     </div>
   )
