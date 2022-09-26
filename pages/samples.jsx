afda96b79d13219b73339cd17d94c71a1e086c6d
patrick mugambi <patrick@kali.kali>
2022-09-26 09:00:00 +0300

    changes

--- pages/samples.jsx
+++ pages/samples.jsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import MoreInfo from '../components/home/MoreInfo'
 import Hero from '../components/samples/Hero'
-export default function Samples() {
+
+export default function Sample() {
   return (
   <div>
 <Hero/>
+
 <MoreInfo/>
     </div>
   )
