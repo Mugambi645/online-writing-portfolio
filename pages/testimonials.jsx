056969c40d1542fbeba3bb1ecb6c9de7aa2083f9
patrick mugambi <patrick@kali.kali>
2022-10-04 03:25:28 +0300

    add more info component in testimonials

--- pages/testimonials.jsx
+++ pages/testimonials.jsx
@@ -1,7 +1,7 @@
 
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import MoreInfo from '../components/home/MoreInfo'
+import MoreInfo from '../components/testimonials/MoreInfo'
 import Hero from '../components/testimonials/Hero'
 import TestimonialComponent from '../components/testimonials/TestimonialComponent'
 export default function Testimonials() {
