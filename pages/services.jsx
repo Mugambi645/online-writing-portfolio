b4433950d05f0ada0d531d29de01390e38cec8aa
patrick mugambi <patrick@kali.kali>
2022-10-04 05:14:40 +0300

    add next-image optized hero section

--- pages/services.jsx
+++ pages/services.jsx
@@ -1,13 +1,13 @@
 
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import MoreInfo from '../components/home/MoreInfo'
+import MoreInfo from '../components/services/MoreInfo'
 import Hero from '../components/services/Hero'
 import ServiceCard from '../components/services/ServiceCard'
 export default function Services() {
   return (
   <div>
-<Hero/>
+<Hero title="Services"/>
 <ServiceCard/>
 <MoreInfo/>
     </div>
