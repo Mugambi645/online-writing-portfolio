86b63b13fa3e61f5395fd4a5c1d92c4cdb3fd37e
patrick mugambi <patrick@kali.kali>
2022-10-11 02:17:22 +0300

    changes

--- components/home/MoreInfo.jsx
+++ components/home/MoreInfo.jsx
@@ -4,7 +4,7 @@ const MoreInfo = () => {
 <section className="py-6 dark:bg-gray-800 dark:text-gray-50">
 	<div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between">
 		<h1 className="text-3xl font-semibold leading-tight text-center lg:text-left">Why i am best suited for your work</h1>
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Why Me</button>
+        <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Why Me</button>
 	</div>
 </section>
         </>
